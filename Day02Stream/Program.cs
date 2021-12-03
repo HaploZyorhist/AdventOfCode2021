9b5d8df701fee718284731ab913ebecebae804e3
Zyorhist <zyorhist@gmail.com>
2021-12-02 20:09:45 -0700

    Day 2 for stream addition

---
+++ Day02Stream/Program.cs
@@ -0,0 +1,26 @@
+﻿using Day02Stream;
+
+SubmarinePositionService supPos = new SubmarinePositionService();
+
+StreamReader sr = File.OpenText(@"..\..\..\Data\Data.txt");
+string data = sr.ReadToEnd();
+
+string[] depths = data.Split("\r\n");
+
+var instructionList = supPos.ProcessData(depths);
+
+int distance;
+int depth;
+int aim;
+int tryAgainDepth;
+int tryAgainDistance;
+(distance, depth) = supPos.MoveTheSub(instructionList);
+(tryAgainDistance, tryAgainDepth, aim) = supPos.TryAgain(instructionList);
+
+Console.WriteLine($"Your submarine finished the instructions at {distance} units from the start, " +
+                  $"and {depth} units deep.  The total returned value is {distance * depth}\r\n");
+
+Console.WriteLine($"Your submarine finished the instructions at {tryAgainDistance} units from the start, " +
+                  $"and {tryAgainDepth} units deep.  Your final aim was {aim}.  The total returned value is {tryAgainDistance * tryAgainDepth}");
+
+Console.ReadKey();
---
+++ Day02Stream/SubmarinePositionService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02Stream
+{
+    public class SubmarinePositionService
+    {
+        public List<(string, int)> ProcessData (string[] data)
+        {
+            var list = new List<(string,int)> ();
+
+            foreach (var item in data)
+            {
+                var d = item.Split(" ");
+                var n = int.Parse(d[1]);
+
+                list.Add((d[0], n));
+            }
+
+            return list;
+        }
+
+        public (int, int) MoveTheSub(List<(string command, int value)> instructions)
+        {
+            int subDistance = 0;
+            int subDepth = 0;
+
+            foreach(var item in instructions)
+            {
+                if(string.Equals(item.command, "forward", StringComparison.OrdinalIgnoreCase))
+                {
+                    subDistance += item.value;
+                }
+                else if(string.Equals(item.command, "down", StringComparison.OrdinalIgnoreCase))
+                {
+                    subDepth += item.value;
+                }
+                else if(string.Equals (item.command, "up", StringComparison.OrdinalIgnoreCase))
+                {
+                    subDepth -= item.value;
+                }
+            }
+
+            return (subDistance, subDepth);
+        }
+
+        public (int, int, int) TryAgain(List<(string command, int value)> instructions)
+        {
+            int subDistance = 0;
+            int subDepth = 0;
+            int subAim = 0;
+
+            foreach (var item in instructions)
+            {
+                if (string.Equals(item.command, "forward", StringComparison.OrdinalIgnoreCase))
+                {
+                    subDistance += item.value;
+                    subDepth += item.value * subAim;
+                }
+                else if (string.Equals(item.command, "down", StringComparison.OrdinalIgnoreCase))
+                {
+                    subAim += item.value;
+                }
+                else if (string.Equals(item.command, "up", StringComparison.OrdinalIgnoreCase))
+                {
+                    subAim -= item.value;
+                }
+            }
+
+            return (subDistance, subDepth, subAim);
+        }
+    }
+}
