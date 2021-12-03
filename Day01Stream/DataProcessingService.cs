8390018a143c21a3da1811d4609d82cc9272df6d
Zyorhist <zyorhist@gmail.com>
2021-12-02 19:41:09 -0700

    day 1 project for stream addition

---
+++ Day01Stream/DataProcessingService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01Stream
+{
+    public class DataProcessingService
+    {
+        public List<int> ConvertToInts (string[] depthsString)
+        {
+            List<int> list = new List<int>();
+
+            foreach(string depth in depthsString)
+            {
+                var d = int.Parse(depth);
+                list.Add(d);
+            }
+
+            return list;
+        }
+
+        public int Part1Processing(List<int> depths)
+        {
+            int depthIncreaseCount = 0;
+
+            for (int i = 1; i < depths.Count; i++)
+            {
+                if(depths[i] > depths[i - 1])
+                {
+                    depthIncreaseCount++;
+                }
+
+            }
+
+            return depthIncreaseCount;
+        }
+
+        public int Part2Processing(List<int> depths)
+        {
+            int depthIncreaseCount = 0;
+
+            for (int i = 3; i < depths.Count; i++)
+            {
+                if ((depths[i] + depths[i - 1] + depths[i - 2]) > (depths[i - 1] + depths[i - 2] + depths[i - 3]))
+                {
+                    depthIncreaseCount++;
+                }
+
+            }
+
+            return depthIncreaseCount;
+        }
+    }
+}
---
+++ Day01Stream/Program.cs
@@ -0,0 +1,17 @@
+﻿using Day01Stream;
+
+DataProcessingService ds = new DataProcessingService();
+
+StreamReader sr = File.OpenText(@"..\..\..\Data\Data.txt");
+string data = sr.ReadToEnd();
+
+string[] depths = data.Split("\r\n");
+
+List<int> dList = ds.ConvertToInts(depths);
+
+var depthIncreaseCount = ds.Part1Processing(dList);
+var depthTripleCount = ds.Part2Processing(dList);
+
+Console.WriteLine($"The depth of the sea has increased {depthIncreaseCount} times");
+Console.WriteLine($"The depth of the sea has increased {depthTripleCount} times for triple measurements");
+Console.ReadKey();
